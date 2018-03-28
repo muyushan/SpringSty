afdad7fd1345abb6873b8a9fcba9b505dbf8084e
muyushan <muyushanm@126.com>
2018-03-29 00:06:28 +0800

    20180329上传文件完成。可以在多Linux系统和Windows系统通用 上传文件后可以直接显示图片

--- src/main/java/com/sane/pkg/controller/FirstController.java
+++ src/main/java/com/sane/pkg/controller/FirstController.java
@@ -7,8 +7,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RequestPart;
+import org.springframework.web.context.ContextLoader;
 import org.springframework.web.multipart.MultipartFile;
 
+import javax.servlet.http.HttpServletRequest;
 import java.io.File;
 import java.io.IOException;
 
@@ -16,17 +18,22 @@ import java.io.IOException;
 public class FirstController {
     private Logger logger = Logger.getLogger(FirstController.class);
     @RequestMapping("/main")
-    public  String main(@RequestPart("pic")MultipartFile pic, String uname){
-        logger.info(pic.getName());
-        logger.info(pic.getOriginalFilename());
-        logger.info(pic.getSize());
+    public  String main(@RequestPart("pic")MultipartFile pic, String uname, HttpServletRequest request){
         try {
-            FileUtils.forceMkdir(new File("D:\\upload\\pic"));
-            FileUtils.writeByteArrayToFile(new File("D:\\upload\\pic\\"+pic.getOriginalFilename()),pic.getBytes());
+
+            String uploadPath=ContextLoader.getCurrentWebApplicationContext().getServletContext().getRealPath("upload"+File.separator+"pic");
+            File filePath=new File(uploadPath);
+            if(!filePath.exists()){
+                FileUtils.forceMkdir(new File(uploadPath));
+            }
+            pic.transferTo(new File(uploadPath+File.separator+pic.getOriginalFilename()));
+            String url= request.getScheme()+"://"+request.getServerName()+":"+request.getServerPort()+request.getContextPath()+File.separator+"upload"+File.separator+"pic"+File.separator+pic.getOriginalFilename();
+
+        logger.info(url);
+
         } catch (IOException e) {
             e.printStackTrace();
         }
-//        pic.transferTo(new File("D:\\"));
         return "main";
     }
 }
