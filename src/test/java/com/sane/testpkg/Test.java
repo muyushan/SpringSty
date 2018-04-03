ba8d51dbd496e643f1358a73dadf973ae31ca782
muys2 <muys2@lenovo.com>
2018-04-03 19:32:39 +0800

    提交一把

---
+++ src/test/java/com/sane/testpkg/Test.java
@@ -0,0 +1,12 @@
+package com.sane.testpkg;
+
+import org.springframework.cache.CacheManager;
+
+public class Test {
+    @org.junit.Test
+    public  void test1(){
+
+
+//        CacheManager cacheManager=
+    }
+}
