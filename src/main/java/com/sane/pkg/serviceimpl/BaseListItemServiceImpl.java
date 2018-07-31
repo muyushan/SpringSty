579db3b704ca9b27a0815d2e0ea9edbdbf1b1373
muyushan <muyushanm@126.com>
2018-07-31 23:36:34 +0800

    BaseListItem增加完成

--- src/main/java/com/sane/pkg/serviceimpl/BaseListItemServiceImpl.java
+++ src/main/java/com/sane/pkg/serviceimpl/BaseListItemServiceImpl.java
@@ -5,6 +5,7 @@ import com.github.pagehelper.PageInfo;
 import com.sane.pkg.beans.BaseListItem;
 import com.sane.pkg.beans.BaseListItemCriteria;
 import com.sane.pkg.beans.BaseListTypeParam;
+import com.sane.pkg.beans.commons.MsgBean;
 import com.sane.pkg.dao.mappers.BaseListItemMapper;
 import com.sane.pkg.dao.mappers.udmappers.BaseListItemUDMapper;
 import com.sane.pkg.service.BaseListItemService;
@@ -22,5 +23,38 @@ public class BaseListItemServiceImpl implements BaseListItemService {
         PageHelper.startPage(baseListTypeParam.getPage(),baseListTypeParam.getLimit());
         PageInfo pageInfo=new PageInfo(baseListItemUdMapper.queryBaseListItemByTypeId(baseListTypeParam.getTypeid()));
         return pageInfo;
+    }
+
+    @Override
+    public MsgBean addBaseListItem(BaseListItem baseListItem) {
+        MsgBean msgBean=new MsgBean();
+        if(isRepeat(baseListItem)){
+            msgBean.setMessage("字典项重复，不允许添加");
+            msgBean.setCode(MsgBean.FAIL);
+            return msgBean;
+        }
+
+        int count=baseListItemMapper.insertSelective(baseListItem);
+
+        if(count==1){
+            msgBean.setCode(MsgBean.SUCCESS);
+        }else{
+            msgBean.setCode(MsgBean.FAIL);
+            msgBean.setMessage("保存失败，请重试");
+        }
+        return msgBean;
+    }
+
+    private  boolean isRepeat(BaseListItem baseListItem){
+        BaseListItemCriteria baseListItemCriteria=new BaseListItemCriteria();
+        BaseListItemCriteria.Criteria criteria=baseListItemCriteria.createCriteria();
+        criteria.andListvalueEqualTo(baseListItem.getListvalue());
+        criteria.andTypeidEqualTo(baseListItem.getTypeid());
+        criteria.andListnameEqualTo(baseListItem.getListname());
+        if(baseListItem.getListid()!=null){
+            criteria.andListidNotEqualTo(baseListItem.getListid());
+        }
+        int count=baseListItemMapper.countByExample(baseListItemCriteria);
+        return  count>0;
     }
 }
