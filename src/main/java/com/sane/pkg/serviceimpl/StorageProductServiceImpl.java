9b0e8c8e1a5c806a1a643f5049a4740c542670be
muyushan <muyushanm@126.com>
2018-09-05 22:42:26 +0800

    基础物料管理开了个头3

--- src/main/java/com/sane/pkg/serviceimpl/StorageProductServiceImpl.java
+++ src/main/java/com/sane/pkg/serviceimpl/StorageProductServiceImpl.java
@@ -1,14 +1,18 @@
 package com.sane.pkg.serviceimpl;
 
+import com.sane.pkg.beans.StorageInOutRecord;
 import com.sane.pkg.beans.StorageProduct;
 import com.sane.pkg.beans.StorageProductCriteria;
 import com.sane.pkg.beans.commons.MsgBean;
 import com.sane.pkg.dao.mappers.StorageProductMapper;
 import com.sane.pkg.exceptions.BizException;
 import com.sane.pkg.service.StorageProductService;
+import com.sane.pkg.utils.SessionUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Date;
 
 @Service
 public class StorageProductServiceImpl implements StorageProductService {
@@ -20,7 +24,24 @@ public class StorageProductServiceImpl implements StorageProductService {
     public MsgBean addStorageProduct(StorageProduct storageProduct) throws BizException, Exception {
        MsgBean msgBean=new MsgBean();
        StorageProductCriteria storageProductCriteria=new StorageProductCriteria();
-//       StorageProductCriteria.Criteria
+       StorageProductCriteria.Criteria storageProductCriteriaSql=storageProductCriteria.createCriteria();
+       storageProductCriteriaSql.andProductCodeEqualTo(storageProduct.getProductCode());
+       storageProductCriteriaSql.andTypeEqualTo(storageProduct.getType());
+       int count=storageProductMapper.countByExample(storageProductCriteria);
+       if(count>0){
+           msgBean.setCode(MsgBean.FAIL);
+           msgBean.setMessage("该物料的此类型库存已经存在无需再次创建");
+           return  msgBean;
+
+       }
+       StorageInOutRecord storageInOutRecord=new StorageInOutRecord();
+       storageInOutRecord.setCreateDate(new Date());
+       storageInOutRecord.setCreator(SessionUtil.getCurrentUserInfo());
+       storageInOutRecord.setFormerQuantity(0);
+       storageInOutRecord.setQuantity(storageProduct.getQuantity().intValue());
+       storageInOutRecord.setInOutType("IN");
+       storageInOutRecord.setInOutCode("");
+
         storageProductMapper.insertSelective(storageProduct);
        return msgBean;
     }
